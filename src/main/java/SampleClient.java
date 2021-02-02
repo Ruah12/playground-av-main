d94f671671612c0211b85659a326c934defbc377
Ruah12 <51417602+Ruah12@users.noreply.github.com>
2021-02-01 19:59:21 -0500

    Update SampleClient.java

--- src/main/java/SampleClient.java
+++ src/main/java/SampleClient.java
@@ -2,11 +2,14 @@ import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
 import ca.uhn.fhir.rest.client.interceptor.LoggingInterceptor;
 import org.hl7.fhir.r4.model.Bundle;
+import org.hl7.fhir.r4.model.DateType;
 import org.hl7.fhir.r4.model.Patient;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
+import java.text.SimpleDateFormat;
 
 public class SampleClient {
 
@@ -15,7 +18,7 @@ public class SampleClient {
         // Create a FHIR client
         FhirContext fhirContext = FhirContext.forR4();
         IGenericClient client = fhirContext.newRestfulGenericClient("http://hapi.fhir.org/baseR4");
-        client.registerInterceptor(new LoggingInterceptor(false));
+        client.registerInterceptor(new LoggingInterceptor(true));
 
         // Search for Patient resources
         Bundle response = client
@@ -30,17 +33,25 @@ public class SampleClient {
         System.out.println(response.getEntry().size());
 
         List<Patient> allPatients = new ArrayList<>();
-        for (Bundle.BundleEntryComponent entryComponent : response.getEntry()) {
+        for (Bundle.BundleEntryComponent entryComponent : response.getEntry())
+        {
             allPatients.add((Patient) entryComponent.getResource());
-
             Patient patient = (Patient) entryComponent.getResource();
-            System.out.println(patient.getName().get(0).getGiven());
         }
 
-        System.out.println("Once again: ");
-        allPatients.sort(Comparator.comparing(a -> a.getName().get(0).getGiven().toString()));
-        for (Patient patient : allPatients) {
-            System.out.println(patient.getName().get(0).getGiven());
+        allPatients.sort(Comparator.comparing(a -> a.getName().get(0).getGivenAsSingleString().toString(), String.CASE_INSENSITIVE_ORDER));
+        for (Patient patient : allPatients)
+        {
+            Date birthDate = patient.getBirthDateElement().getValue();
+            String stringDate = "Not Specified.";
+            if(birthDate != null)
+            {
+                SimpleDateFormat DateFor = new SimpleDateFormat("dd/MM/yyyy");
+                stringDate = DateFor.format(birthDate);
+            }
+
+            System.out.println(patient.getName().get(0).getGivenAsSingleString() + " " +
+                    patient.getName().get(0).getFamilyElement()+ " BirthDate: " + stringDate);
         }
     }
 
